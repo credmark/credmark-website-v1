cd090fa8c7e5e940f25dce1e5997d97f0398cb3c
Elektra Murphy <elektra.murphy@icloud.com>
2021-09-16 15:38:19 +0100

    font url corrected

--- credmark-website/components/pages/whitepaper.js
+++ credmark-website/components/pages/whitepaper.js
@@ -6,7 +6,7 @@ export default function Whitepaper() {
         <>
             <main className="flex-1 relative overflow-y-auto focus:outline-none rounded-md ">
                     <div className="rounded-md">
-                        <div className="px-5 py-10 md:pt-28 md:px-40">
+                        <div className="px-5 py-10 md:pt-28 md:px-40 homeTop">
                                 <h1 className="pt-5 font-light text-5xl md:pt-0">
                                     WHITE PAPER
                                 </h1>
@@ -15,7 +15,7 @@ export default function Whitepaper() {
                                 <p className="italic pt-5">Published May 17, 2021</p>
                                 <a href="https://reports.credmark.com/whitepaper.pdf" target="_blank" rel="norefferer"><img width={350} className="block m-auto" src="/assets/credmark-white-paper.png" alt="Credmark White Paper"></img></a>
                         </div>
-                        <div className="grid grid-cols-1 md:grid-cols-3 block m-auto">
+                        <div className="grid grid-cols-1 md:grid-cols-3 block m-auto homeBottom">
                         <a href="https://reports.credmark.com/litepaper.pdf" target="_blank" rel="norefferer"><img width={350} className="block m-auto" src="/assets/credmark-english-white-paper.png" alt="Credmark White Paper"></img></a>
                         <a href="https://reports.credmark.com/whitepaper.pdf" target="_blank" rel="norefferer"><img width={350} className="block m-auto" src="/assets/credmark-tc-white-paper.png" alt="Credmark White Paper"></img></a>
                         <a href="https://storage.googleapis.com/credmark-cryptocreditreports/Credmark%20Litepaper%20Simplified%20Chinese%20%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87.pdf" target="_blank" rel="norefferer"><img width={350} className="block m-auto" src="/assets/credmark-sc-white-paper.png" alt="Credmark White Paper"></img></a>
