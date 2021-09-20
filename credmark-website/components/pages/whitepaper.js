7a82d24c427fdc7932ee5b9b7b4acc54a5167b49
Elektra Murphy <elektra.murphy@icloud.com>
2021-09-20 16:59:09 -0400

    Space on Whitepaper adjusted

--- credmark-website/components/pages/whitepaper.js
+++ credmark-website/components/pages/whitepaper.js
@@ -4,7 +4,7 @@ export default function Whitepaper() {
         <>
             <main className="flex-1 relative overflow-y-auto focus:outline-none rounded-md">
                     <div className="rounded-md trianglePaper">
-                        <div className="px-5 py-10 md:pt-28 md:px-40">
+                        <div className="px-5 py-10 md:pt-28 md:px-28">
                                 <h1 className="pt-5 font-light text-4xl md:text-5xl md:pt-0">
                                     WHITE PAPER
                                 </h1>
@@ -13,10 +13,10 @@ export default function Whitepaper() {
                                 <p className="italic pt-5">Published May 17, 2021</p>
                                 <a href="https://reports.credmark.com/whitepaper.pdf" target="_blank" rel="noreferrer"><img width={350} className="block m-auto" src="/assets/credmark-white-paper.png" alt="Credmark White Paper"></img></a>
                         </div>
-                        <div className="grid grid-cols-1 md:grid-cols-3 block m-auto pb-20">
-                        <a href="https://reports.credmark.com/litepaper.pdf" target="_blank" rel="noreferrer"><img width={350} className="block m-auto" src="/assets/credmark-english-white-paper.png" alt="Credmark White Paper"></img></a>
-                        <a href="https://storage.googleapis.com/credmark-cryptocreditreports/Credmark%20Litepaper%20Simplified%20Chinese%20%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87.pdf" target="_blank" rel="noreferrer"><img width={350} className="block m-auto" src="/assets/credmark-tc-white-paper.png" alt="Credmark White Paper"></img></a>
-                        <a href="https://storage.googleapis.com/credmark-cryptocreditreports/Credmark%20Litepaper%20Simplified%20Chinese%20%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87.pdf" target="_blank" rel="noreferrer"><img width={350} className="block m-auto" src="/assets/credmark-sc-white-paper.png" alt="Credmark White Paper"></img></a>
+                        <div className="flex block m-auto pb-20 px-20">
+                        <a href="https://reports.credmark.com/litepaper.pdf" target="_blank" rel="noreferrer"><img id="whitepaper" src="/assets/credmark-english-white-paper.png" alt="Credmark White Paper"></img></a>
+                        <a href="https://storage.googleapis.com/credmark-cryptocreditreports/Credmark%20Litepaper%20Simplified%20Chinese%20%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87.pdf" target="_blank" rel="noreferrer"><img id="whitepaper" src="/assets/credmark-tc-white-paper.png" alt="Credmark White Paper"></img></a>
+                        <a href="https://storage.googleapis.com/credmark-cryptocreditreports/Credmark%20Litepaper%20Simplified%20Chinese%20%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87.pdf" target="_blank" rel="noreferrer"><img id="whitepaper" src="/assets/credmark-sc-white-paper.png" alt="Credmark White Paper"></img></a>
                         </div>
                     </div>
                 </main>
