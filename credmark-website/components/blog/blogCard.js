7feba0a97a23e1bf45e3da11f43438d3b15681c5
Elektra Murphy <elektra.murphy@icloud.com>
2021-09-21 06:28:53 -0400

    Blog card height and width updated

--- credmark-website/components/blog/blogCard.js
+++ credmark-website/components/blog/blogCard.js
@@ -5,12 +5,12 @@ const BlogCard = (props) => {
     return (
         <>
             <div className="mt-10 md:mt-0 block m-auto max-w-3xl">
-                <div className="grid grid-cols-1 md:grid-cols-2 py-2.5">
+                <div className="grid grid-cols-1 md:grid-cols-2 py-2.5 max-w-lg block m-auto">
                     <Link href={`/blog/${props.slug}`}>
                         <img
                             alt={props.alt}
                             src={props.img}
-                            className='w-96 h-72 rounded-3xl cursor-pointer'
+                            className='w-64 h-52 rounded-3xl cursor-pointer'
                             id="blogPage"
                         />
                     </Link>
