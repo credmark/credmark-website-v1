d09f6ff9fe9fc2c53ee2afae094602cebd40ce3d
Elektra Murphy <elektra.murphy@icloud.com>
2021-09-22 12:49:41 -0400

    Whitepaper logos made smaller

--- credmark-website/components/pages/whitepaper.js
+++ credmark-website/components/pages/whitepaper.js
@@ -2,24 +2,22 @@ export default function Whitepaper() {
 
     return (
         <>
-            <main className="flex-1 relative overflow-y-auto focus:outline-none rounded-md bg-white h-screen">
-                    <div className="rounded-md">
-                        <div className="px-5 py-10 md:pt-28 md:px-28">
-                                <h1 className="pt-5 font-light text-4xl md:text-5xl md:pt-0">
-                                    WHITE PAPER
-                                </h1>
-                                <p className="py-5">Click below to explore our White Paper and Lite Paper. </p>
-                                <p>Our White Paper offers an in-depth look into the Credmark platform - how we generate high integrity data, how our models are built and delivered, and how community-driven governance oversees its maintenance and growth. </p>
-                                <p className="italic pt-5">Published May 17, 2021</p>
-                                <a href="https://reports.credmark.com/whitepaper.pdf" target="_blank" rel="noreferrer"><img width={350} className="block m-auto" src="/assets/credmark-white-paper.png" alt="Credmark White Paper"></img></a>
-                        </div>
-                        <div className="flex block m-auto pb-20 px-20">
+            <main className="flex-1 relative overflow-y-auto focus:outline-none rounded-md bg-white">
+                    <div className="px-5 py-10 md:pt-28 md:px-28">
+                        <h1 className="pt-5 font-light text-4xl md:text-5xl md:pt-0">
+                            WHITE PAPER
+                        </h1>
+                        <p className="py-5">Click below to explore our White Paper and Lite Paper. </p>
+                        <p>Our White Paper offers an in-depth look into the Credmark platform - how we generate high integrity data, how our models are built and delivered, and how community-driven governance oversees its maintenance and growth. </p>
+                        <p className="italic pt-5">Published May 17, 2021</p>
+                        <a href="https://reports.credmark.com/whitepaper.pdf" target="_blank" rel="noreferrer"><img width={300} className="block m-auto" src="/assets/credmark-white-paper.png" alt="Credmark White Paper"></img></a>
+                    </div>
+                    <div className="flex block m-auto pb-20 md:px-40">
                         <a href="https://reports.credmark.com/litepaper.pdf" target="_blank" rel="noreferrer"><img id="whitepaper" src="/assets/credmark-english-white-paper.png" alt="Credmark White Paper"></img></a>
                         <a href="https://storage.googleapis.com/credmark-cryptocreditreports/Credmark%20Litepaper%20Simplified%20Chinese%20%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87.pdf" target="_blank" rel="noreferrer"><img id="whitepaper" src="/assets/credmark-tc-white-paper.png" alt="Credmark White Paper"></img></a>
                         <a href="https://storage.googleapis.com/credmark-cryptocreditreports/Credmark%20Litepaper%20Simplified%20Chinese%20%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87.pdf" target="_blank" rel="noreferrer"><img id="whitepaper" src="/assets/credmark-sc-white-paper.png" alt="Credmark White Paper"></img></a>
-                        </div>
                     </div>
-                </main>
+            </main>
         </>
 
     )
