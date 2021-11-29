329441990d937bf8b9237c94a551347918179880
Elektra Murphy <elektra.murphy@icloud.com>
2021-11-29 21:22:24 +0000

    Subscribe Mobile optimised

--- credmark-website/components/subscribe/subscribe.js
+++ credmark-website/components/subscribe/subscribe.js
@@ -31,7 +31,9 @@ const Subscribe = () => {
                         <div className="ml-15">
                             <p className="credmark text-center md:text-left text-white text-base pt-16 md:pt-0">Newsletter</p>
                             <p className="text-white text-center md:text-left text-base">For the latest partnerships, community updates and more</p>
+                            <div className="flex justify-center md:text-left md:block">
                             <a href="https://credmark.us19.list-manage.com/subscribe?u=b6dfe393113892a064b435044&id=c295a3edd2" target="blank" rel="noreferrer"><p className="text-white text-center md:text-left text-base float-left pt-5">Subscribe<img width={40} className="float-right pt-0.5 pl-5" alt="arrow right" src="../assets/arrow-white.svg" /></p></a>
+                            </div>
                         </div>
                     </div>
                 </div>
